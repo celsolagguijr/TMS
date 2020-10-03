84ca44c8f742f64d9532a52ed6d014b0a193ae8b
celsolaggui@gmail.com <celsolaggui@gmail.com>
2020-10-03 11:22:25 +0800

    Sequelize file structure (.sequelizerc) and environment variable added (.env)

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,14 @@
-{
+
+const path = require('path');
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "sampleDB",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username"  : process.env.DB_USERNAME,
+    "password"  : process.env.DB_PASSWORD,
+    "database"  : process.env.DB_NAME,
+    "host"      : process.env.DB_HOST,
+    "dialect"   : process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
